227f96f37c5403a2ca93f9cf281499bcb6e2a324
Zach <zachary.lynch1337@gmail.com>
2020-10-18 23:44:53 -0700

    created routes for books

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/delete'
+  get '/books', to: 'books#index' as: 'books' 
+  get '/books/:id', to: 'books#show', as: 'book'
+  get '/books/new', to: 'books#new', as: 'new_book'
+  post '/books', to: 'books#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
