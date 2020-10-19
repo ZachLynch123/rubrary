199de0e275c1c7845f69d4ceacf4518b2a00064f
Zach <zachary.lynch1337@gmail.com>
2020-10-18 23:38:36 -0700

    fixed naming conventions with books and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'book/index'
-  get 'book/show'
-  get 'book/new'
-  get 'book/create'
-  get 'book/edit'
-  get 'book/delete'
+
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
