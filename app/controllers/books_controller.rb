e3e30a31d45f6cac7ffbad30a5b8c265a6cac7a3
Zach <zachary.lynch1337@gmail.com>
2020-10-18 23:56:30 -0700

    created a few actions in the books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,44 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all 
   end
 
   def show
+    @book = find_book
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    Book.create(book_params)
+    redirect_to books_path
   end
 
   def edit
+    @book = find_book
   end
+
+  def update
+    @book = find_book
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
 
   def delete
   end
+
+  private 
+
+  def find_book 
+    return Book.find(params[:id])
+  end
+
+  def book_params
+    return params.require(:book).permit(:title, :description)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/books/:id', to: 'books#show', as: 'book'
   get '/books/new', to: 'books#new', as: 'new_book'
   post '/books', to: 'books#create'
+  get '/books/:id/edit', to: 'items#edit', as: 'edit_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
