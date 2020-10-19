88e8d8136fdaa4d56a09c061bb50a7483b420edc
Zach <zachary.lynch1337@gmail.com>
2020-10-18 23:34:12 -0700

    Created controllers and views for user and books

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'book/index'
+  get 'book/show'
+  get 'book/new'
+  get 'book/create'
+  get 'book/edit'
+  get 'book/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
