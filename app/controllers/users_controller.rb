984faba817f786105323118b4d721e776dbefebc
Zach <zachary.lynch1337@gmail.com>
2020-10-21 15:54:24 -0700

    creating signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
 
+    def new 
+        @user = User.new 
+    end
+
+    def create 
+        @user = User.create(user_params)
+
+        if @user.save 
+            redirect_to @user 
+        else 
+            render :new 
+        end
+
+    end
+
+
+
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:email, :password)
+    end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   #session
-
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'session#destroy'
