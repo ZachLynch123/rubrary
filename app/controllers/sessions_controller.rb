bd14b4d3714d38560fadcc0809249320b8e5a25d
Zach <zachary.lynch1337@gmail.com>
2020-10-22 23:45:36 -0700

    Refactored sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,25 +6,10 @@ class SessionsController < ApplicationController
     def create 
         # OAuth login
         if auth_hash = request.env["omniauth.auth"]
-            git_email = auth_hash["info"]["email"]
-           # If user already exists in the db
-           if @user = User.find_by(:email => git_email)
-            session[:user_id] = @user.id 
-            byebug
-           else 
+            user = User.find_or_create_by_omniauth(auth_hash)
+            session[:user_id] = user.id
             redirect_to root_path 
         else
-            # User doesn't exist in db yet 
-            @user = User.new(:email => git_email, :password => SecureRandom.hex)
-            byebug
-            if @user.save 
-                session[:user_id] = @user.id 
-                redirect_to @user 
-            else
-                raise user.errors.full_messages.inspect 
-            end
-        end
-    else
             # Normal Login
             @user = User.find_by(email: params[:email])
             if @user && @user.authenticate(params[:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,20 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    
+    def self.find_or_create_by_omniauth(auth_hash)
+
+        auth_email = request.env["omniauth.auth"]["info"]["email"]
+
+        if user = User.find_by(:email => auth_email)
+            session[:user_id] = user.id 
+            redirect_to user 
+        else 
+            user = User.new(:email => auth_email, :password => SecureRandom.hex)
+            if user.save 
+                session[:user_id] = user.id 
+                redirect_to user 
+            else 
+                raise user.errors.full_message.inspect 
+            end
+        end
 end
