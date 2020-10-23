4f4470890ea4cf874ef5ec89a81b4530a11ddd6f
Zach <zachary.lynch1337@gmail.com>
2020-10-23 00:36:33 -0700

    created an index page and a new page for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+
+    def index 
+        @comments = Comment.where(book_id: params[:book_id])
+    end
+
+    def new 
+        comment = Comment.new
+    end
+
+    def create 
+
+    end
+
+
+
 end
