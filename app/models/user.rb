757680183294da0667ea6b4dbf1dce1638a97bf7
Zach <zachary.lynch1337@gmail.com>
2020-11-09 10:22:23 -0800

    Errors now appear in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
 
     validates :email, presence: true
     # Validates that email is unique and has an '@' sign in it
-    validates :email,
-    format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 4, maximum: 254 }  
+    validates :email,format: { with: /(.+)@(.+)/, message: "Email invalid"  }
+    
 end
